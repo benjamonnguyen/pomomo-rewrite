6ed3f35ce07aeb7e9bf7b8df1b29d8d969eaa191
benjamonnguyen <43457281+benjamonnguyen@users.noreply.github.com>
2022-09-21 23:38:20 -0700

    minor config rename

--- pomomo-bot/src/db/redis-clients.ts
+++ pomomo-bot/src/db/redis-clients.ts
@@ -2,6 +2,6 @@ import { createClient } from 'redis';
 import config from 'config';
 
 export const sessionsClient = createClient({
-	url: config.get('redis.sessions-db.url'),
+	url: config.get('redis.db.sessions.url'),
 });
 sessionsClient.on('error', (err) => console.log('Redis Client Error', err));
